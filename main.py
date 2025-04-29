0a60ac24c37783d2a61068f8e1a8ccd466e5eaa7
SaalihahWebDev <bashirabanu91@gmail.com>
2025-04-29 06:13:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+light=int(input("Enter a number to On or Off the lights: "))
+if light==1:
+    print("Light is on💡💡💡")
+elif light==0:
+    print("Light is Off💔💔")
+else:
+    print("Write only 0s and 1")
